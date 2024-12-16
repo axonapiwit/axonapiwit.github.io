cdce5cf887de421aee8289737f416711f08f4ea6
axonapiwit <axon.apiwit@gmail.com>
2024-12-16 22:31:54 +0700

    fix ci cd

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Enable static export
+  assetPrefix: './', // Use relative paths for assets
+  basePath: '/axonapiwit.github.io',
+  images: {
+    unoptimized: true, // Disable image optimization for GitHub Pages
+  },
 };
 
 export default nextConfig;
